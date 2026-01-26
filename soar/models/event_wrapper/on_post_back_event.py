d49a57e1c31416bf47dad6926deed1ff841ebce1
ShirayukiRin <ShirashimiLin@gmail.com>
2026-01-26 22:07:30 +0800

    update postback_event wrapper and quick_reply builder

--- soar/models/event_wrapper/on_post_back_event.py
+++ soar/models/event_wrapper/on_post_back_event.py
@@ -1,4 +1,6 @@
 import json
+from dataclasses import dataclass
+from datetime import datetime
 
 from linebot.v3.messaging import MessagingApi
 from linebot.v3.webhooks.models.postback_event import PostbackEvent
@@ -6,15 +8,35 @@ from linebot.v3.webhooks.models.postback_event import PostbackEvent
 from soar.models.event_wrapper.keyed_event import KeyedEvent
 
 
+@dataclass
+class DatetimePickerActionResult:
+    mode: str
+    format: str
+    result_str: str
+    datetime: datetime
+
+
 class OnPostBackEvent(KeyedEvent):
     def __init__(self, original_event: PostbackEvent, line_bot_api: MessagingApi):
+        self._json_param = original_event.postback.params
         self._json_data = json.loads(original_event.postback.data)
-        key = self._json_data.get("handler_keyword")
+        key = self._json_data.get("handler_key")
 
         super().__init__(original_event.reply_token, line_bot_api, original_event, key)
 
-    def get_handler_keyword(self) -> str | None:
-        return self._json_data.get("handler_keyword")
+    def get_handler_key(self) -> str | None:
+        return self._json_data.get("handler_key")
 
     def get_data_content(self) -> dict:
         return self._json_data.get("content")
+
+    def get_params(self) -> dict:
+        return self._json_param
+
+    def get_datetime_picker_action_result(self) -> DatetimePickerActionResult | None:
+        if not self._json_data["is_datetime_picker"]:
+            return None
+        time_mode = self._json_data["datetime_picker_mode"]
+        time_format = self._json_data["datetime_picker_format"]
+        result = self._json_param.get(time_mode)
+        return DatetimePickerActionResult(time_mode, time_format, result, datetime.strptime(result, time_format))
--- soar/models/quick_reply_builder.py
+++ soar/models/quick_reply_builder.py
@@ -1,15 +1,11 @@
-import json
-
-from linebot.v3.messaging import PostbackAction
-from linebot.v3.messaging.models import QuickReply, MessageAction, QuickReplyItem, Action
-from pydantic import StrictStr
+from linebot.v3.messaging.models import QuickReply, QuickReplyItem, Action
 
 
 class QuickReplyBuilder:
     def __init__(self):
         self.__items = []
 
-    def __add_item(self, action: Action, image_url: str = None):
+    def add_option(self, action: Action, image_url: str = None):
         self.__items.append(
             QuickReplyItem(
                 action=action,
@@ -17,34 +13,6 @@ class QuickReplyBuilder:
                 imageUrl=image_url,
             ),
         )
-
-    def add_message_action(self, label: str, text: str, image_url: str = None):
-        self.__add_item(MessageAction(label=StrictStr(label), text=StrictStr(text)), image_url=image_url)
-
-    def add_postback_action(self, label: str, data_handler_keyword: str, data_content: dict, display_text: str = None,
-                            input_option: str = None, fill_in_text: str = None, image_url: str = None):
-        """
-
-        :param label:
-        :param data_handler_keyword:
-        :param data_content:
-        :param display_text: User 點 post back action 後 會傳的訊息
-        :param input_option:
-        :param fill_in_text:
-        :param image_url:
-        :return:
-        """
-        self.__add_item(PostbackAction(label=StrictStr(label),
-                                       data=json.dumps(
-                                           {
-                                               "handler_keyword": data_handler_keyword,
-                                               "content": data_content,
-                                           }
-                                       ),
-                                       displayText=None if display_text is None else StrictStr(display_text),
-                                       inputOption=None if input_option is None else StrictStr(input_option),
-                                       fillInText=None if fill_in_text is None else StrictStr(fill_in_text),
-                                       ), image_url=image_url)
 
     def build(self):
         return QuickReply(
