b67e931f47e677b396911e9c8749b5d7544c4357
ShirayukiRin <ShirashimiLin@gmail.com>
2026-01-26 23:59:39 +0800

    move action_object_wrapper.py to utils

--- soar/plugins/hello_world/main.py
+++ soar/plugins/hello_world/main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from soar.core.plugin_event_manager import on_message, on_postback, on_follow
-from soar.models.action_object_wrapper import create_message_action, create_postback_action, create_uri_action, \
+from soar.utils.action_object_wrapper import create_message_action, create_postback_action, create_uri_action, \
     create_datetime_picker_action
 from soar.models.event_wrapper.on_follow_event import OnFollowEvent
 from soar.models.event_wrapper.on_message_event import OnMessageEvent
