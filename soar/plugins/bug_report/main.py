797e840cc1f1a50935f46747fd4e6d2a261d735f
ShirayukiRin <ShirashimiLin@gmail.com>
2026-02-02 16:10:32 +0800

    remove the bug_report plugin

--- soar/plugins/bug_report/main.py
+++
@@ -1,9 +0,0 @@
-from soar.core.plugin_event_manager import on_message
-from soar.models.event_wrapper.on_message_event import OnMessageEvent
-
-
-@on_message.add_handler(key="問題回報")
-def bug_report(event: OnMessageEvent):
-    # TODO bug report
-    event.add_text_message("please report bug")
-    event.submit_reply()
