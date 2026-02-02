90705522721167862edd4909731e4e5b032796a3
ShirayukiRin <ShirashimiLin@gmail.com>
2026-02-02 16:17:36 +0800

    update the on_follow plugin

--- soar/plugins/on_follow/main.py
+++ soar/plugins/on_follow/main.py
@@ -4,5 +4,10 @@ from soar.models.event_wrapper.on_follow_event import OnFollowEvent
 
 @on_follow.add_handler()
 def on_follow(event: OnFollowEvent):
-    event.add_text_message("Welcome")
+    event.add_text_message(
+"""啾！我是高師校園小飛燕 🐦
+由 GDG on Campus NKNU 社團所開發的 LINE Bot
+
+點選下方選單看看現在我可以幫到你什麼～    
+""")
     event.submit_reply()
